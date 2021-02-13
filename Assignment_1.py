c0299f6a3b74effeae2d4f0dbf8b055cbfe7e0be
Preet Patel <preet.makz@gmail.com>
2021-02-13 21:18:14 +0530

    Optimized Assignment 1

--- Assignment_1.py
+++ Assignment_1.py
@@ -19,20 +19,21 @@ class PreProcess:
     def __init__(self,Dir):
         #1. Read and combine the text between the <TEXT> tags by using Beautiful Soup from all the documents.
         self.Dir=Dir
-        self.data=self.fetchComb()
-        self.dataFiltered=self.remPunNum()
-        self.dataTokenSimple=self.dataTokenS()
-        self.dataTokenNLTK=self.dataTokenN()
-        self.lowCase()
-        self.dataStop=self.remStop()
-        self.dataStem=self.Stemming()
-        self.dataLem=self.Lemmatization()
+        #self.data=self.fetchComb()
+        #self.dataFiltered=self.remPunNum()
+        #self.dataTokenSimple=self.dataTokenS()
+        #self.dataTokenNLTK=self.dataTokenN()
+        #self.lowCase()
+        #self.dataStop=self.remStop()
+        #self.dataStem=self.Stemming()
+        #self.dataLem=self.Lemmatization()
         
         #return self.dataLem
     
     def fetchComb(self):
 #         if self.Dir[-1]!="/":
 #             self.Dir+='/'
+        global data
         data=''
         #for i in os.listdir(self.Dir):
         with open(self.Dir) as buff:
@@ -42,32 +43,42 @@ class PreProcess:
     
     #2. Remove punctuation and numerical values from your text.
     def remPunNum(self):
+        self.fetchComb()
+        global dataFiltered
         dataFiltered=''
-        for i in self.data:
+        for i in data:
             if (i not in string.punctuation) and (i not in string.digits):
                 dataFiltered+=i
         return dataFiltered
     
     #3. Perform tokenization on the text.
     def dataTokenS(self):
+        self.remPunNum()
         #simple method
-        dataTokenSimple=self.dataFiltered.split()
+        global dataTokenSimple
+        dataTokenSimple=dataFiltered.split()
         return dataTokenSimple
     
     def dataTokenN(self):
+        self.remPunNum()
         #NLTK method
-        dataTokenNLTK=word_tokenize(self.dataFiltered)
+        global dataTokenNLTK
+        dataTokenNLTK=word_tokenize(dataFiltered)
         return dataTokenNLTK
     
     #4. Convert the text to lowercase
     def lowCase(self):
-        for i in range(len(self.dataTokenNLTK)):
-            self.dataTokenNLTK[i]=self.dataTokenNLTK[i].lower()
+        self.dataTokenN()
+        global dataTokenNLTK
+        for i in range(len(dataTokenNLTK)):
+            dataTokenNLTK[i]=dataTokenNLTK[i].lower()
     
     #5. Remove stop-words from the text
     def remStop(self):
+        self.lowCase()
+        global dataStop
         dataStop=[]
-        for i in self.dataTokenNLTK:
+        for i in dataTokenNLTK:
             if i not in stopwords.words('english'):
                 dataStop.append(i)
         return dataStop
@@ -75,14 +86,18 @@ class PreProcess:
     #6. Perform Stemming (use Porter Stemmer) and Lemmatization (separately) and display the differences that
     #you observe while using the two approaches. (For this you have to use NLTK library).
     def Stemming(self):
+        self.remStop()
+        global dataStem
         dataStem=[]
-        for i in self.dataStop:
+        for i in dataStop:
             dataStem.append(PorterStemmer().stem(i))
         return dataStem
             
     def Lemmatization(self):
+        self.remStop()
+        global dataLem
         dataLem=[]
-        for i in self.dataStop:
+        for i in dataStop:
             dataLem.append(WordNetLemmatizer().lemmatize(i))
         return dataLem
 
